2d596b9327c943bd6a0c149dea58b54c43e71e17
203AKSAM_MUTLU <mutlu.e@hotmail.com>
2022-03-24 20:31:44 +0300

    new

--- Project.API/Startup.cs
+++ Project.API/Startup.cs
@@ -66,7 +66,7 @@ namespace Project.API
                 };
             });
         }
-
+        //
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
